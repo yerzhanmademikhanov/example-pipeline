a3fd5558f32bee75501378ad85a94bb0a345a5f1
Yerzhan Mademikhanov <yerzhanmademikhanov@yerzhanmademikhanov-mac-c02x36w0jgh6.corp.improbable.io>
2018-09-14 15:33:02 +0100

    debiug

--- script2.sh
+++ script2.sh
@@ -1,5 +1,19 @@
-set -e
+#!/bin/bash
 
-echo "Hello World!"
+# Note that we don't enable the 'e' option, which would cause the script to
+# immediately exit if 'run_tests' failed
+set -uox pipefail
 
-echo $YERZHAN
+echo "$PATH"
+PATH_CHECK_STATUS=$?
+
+sh ./run_tests.sh
+TESTS_EXIT_STATUS=$?
+
+sh ./clean_up.sh
+
+env | grep -i -E 'git|node'
+
+
+echo $TESTS_EXIT_STATUS
+echo $PATH_CHECK_STATUS
